a497f8f4e37a111afc368705c8f8c4fe42419c73
aritzg <galdos.aritz@gmail.com>
2016-05-24 12:46:43 +0200

    Created App Manager portlet

---
+++ docroot/WEB-INF/src/net/indaba/lostandfound/portlet/AppManagerPortlet.java
@@ -0,0 +1,11 @@
+package net.indaba.lostandfound.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class AppManagerPortlet
+ */
+public class AppManagerPortlet extends MVCPortlet {
+ 
+
+}
