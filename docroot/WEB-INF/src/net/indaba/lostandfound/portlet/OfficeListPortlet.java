5850a42c1e3710dd902425aa536a3c9a3ba05770
aritzg <galdos.aritz@gmail.com>
2016-09-19 15:00:53 +0200

    creado portlet listado de oficinas

---
+++ docroot/WEB-INF/src/net/indaba/lostandfound/portlet/OfficeListPortlet.java
@@ -0,0 +1,11 @@
+package net.indaba.lostandfound.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class OfficeListPortlet
+ */
+public class OfficeListPortlet extends MVCPortlet {
+ 
+
+}
